45277953f357f9fda50712cd87aafa9dd10ead1f
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-09-26 16:16:25 +0200

    Added _redirects file

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/phonebook-react-refactored.netlify.app/">
+        <BrowserRouter basename="/">
           <App />
         </BrowserRouter>
       </PersistGate>
