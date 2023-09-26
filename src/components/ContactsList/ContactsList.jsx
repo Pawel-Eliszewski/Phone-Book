29c5dc24e3bd996537d06ba39e0ce3fd90494c31
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-09-26 16:24:02 +0200

    Small fix

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,13 +1,13 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   selectError,
   selectContacts,
   selectFilter,
-} from '../../redux/contacts/selectors';
-import { fetchContacts, deleteContact } from '../../redux/contacts/operations';
-import { contactsAmount } from '../../utils/notifications';
-import css from './ContactsList.module.css';
+} from "../../redux/contacts/selectors";
+import { fetchContacts, deleteContact } from "../../redux/contacts/operations";
+import { contactsAmount } from "../../utils/notifications";
+import css from "./ContactsList.module.css";
 
 export const ContactsList = () => {
   const dispatch = useDispatch();
@@ -20,26 +20,28 @@ export const ContactsList = () => {
     contactsAmount(contacts.length);
   }, [contacts.length, dispatch]);
 
-  const filteredContacts = contacts.filter(contact =>
+  const filteredContacts = contacts.filter((contact) =>
     contact.name.toLocaleUpperCase().includes(filter.trim())
   );
 
-  const handleDelete = id => dispatch(deleteContact(id));
+  const handleDelete = (id) => dispatch(deleteContact(id));
 
   return (
     <div className={css.container}>
       <ul className={css.list}>
         {error && <p>{error}</p>}
         {contacts.length > 0 ? (
-          filteredContacts.map(contact => {
+          filteredContacts.map((contact) => {
             return (
               <div className={css.wrapper} key={contact.id}>
                 <li className={css.name}>
-                  {`${contact.name}`}{' '}
+                  {`${contact.name}`}{" "}
                   <dt>
                     <a
                       className={css.phone}
                       href={`tel:${contact.number}`}
+                      target="_blank"
+                      rel="nofollow noopener noreferrer"
                     >{`${contact.number}`}</a>
                   </dt>
                 </li>
@@ -50,7 +52,7 @@ export const ContactsList = () => {
                 >
                   <img
                     className={css.icon}
-                    src={'assets/deleteIconRed.png'}
+                    src={"assets/deleteIconRed.png"}
                     alt="Delete contact"
                   />
                 </button>
